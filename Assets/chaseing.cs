bc08ea79347fdc688ab2a4bdc0317b708203dfeb
iYazeed <ksayazed183@gmail.com>
2025-07-28 21:09:45 +0300

    enemy chase

--- Assets/chaseing.cs
+++ Assets/chaseing.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class chaseing : MonoBehaviour
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyChase : MonoBehaviour
 {
-    public Transform player;
+    [Tooltip("Drag your Player Transform here, or leave blank to auto-find by the 'Player' tag.")]
+    [SerializeField] private Transform player;
+
     private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-
         if (player == null)
-            player = GameObject.FindGameObjectWithTag("Player").transform;
+        {
+            GameObject found = GameObject.FindGameObjectWithTag("Player");
+            if (found != null)
+            {
+                player = found.transform;
+            }
+            else
+            {
+                Debug.LogError($"[{nameof(EnemyChase)}] No Player found! Make sure your Player GameObject is tagged 'Player' or assign it in the Inspector.", this);
+            }
+        }
     }
 
     void Update()
     {
-        if (player != null)
+        if (agent != null && player != null)
         {
             agent.SetDestination(player.position);
         }
