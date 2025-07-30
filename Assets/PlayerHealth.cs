9d07644df61ddb6f710e8a7f17141f319356c32b
iYazeed <ksayazed183@gmail.com>
2025-07-30 20:35:31 +0300

    Player animator , main menu added

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,25 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
 
-    public Slider healthSlider; // «”Õ» «·⁄‰’— „‰ Canvas Â‰«
+    public Slider healthSlider;
+    public GameObject gameOverUI;
+    private Animator animator;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthSlider.maxValue = maxHealth;
         healthSlider.value = currentHealth;
+
+        animator = GetComponent<Animator>();
+        gameOverUI.SetActive(false); // ‰Œ›Ì Ê«ÃÂ… «·‰Â«Ì… »«·»œ«Ì…
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-
         healthSlider.value = currentHealth;
         Debug.Log("Player Health: " + currentHealth);
 
@@ -32,6 +37,18 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         Debug.Log("Player died!");
-        // Â‰«  ﬁœ—  ÷Ì› √‰„Ì‘‰ «·„Ê  √Ê ≈⁄«œ…  ‘€Ì· «·„‘Âœ
+        animator.SetTrigger("Die"); //  ›⁄Ì· √‰„Ì‘‰ «·„Ê 
+        gameOverUI.SetActive(true); // ⁄—÷ Ê«ÃÂ… «·‰Â«Ì…
+    }
+
+    // ÊŸ«∆› «·√“—«—
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Ì‘ €· ›ﬁÿ ›Ì «·‹ Build
     }
 }
